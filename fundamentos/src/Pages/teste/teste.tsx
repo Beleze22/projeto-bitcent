bc9240203f82d8e89489714774366ba8f70ac3f0
Gustavo Beleze <Gustavo Beleze>
2023-11-13 12:05:02 -0300

    Aula 19

---
+++ fundamentos/src/Pages/teste/teste.tsx
@@ -0,0 +1,16 @@
+import { Icon24Hours } from "@tabler/icons-react";
+
+export default function Index() {
+  return (
+    <div
+      className={`
+    flex justify-center items-center h-screen
+    bg-black text-white
+    text-6xl
+    `}
+    >
+      <Icon24Hours size={100} strokeWidth={0.5} />
+      <span>Teste</span>
+    </div>
+  );
+}
